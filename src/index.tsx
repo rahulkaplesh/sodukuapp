4eaa942edb2a3de49774dbccec5da46a533db280
RahulKaplesh <rahulkaplesh@gmail.com>
2020-05-31 18:40:12 +0530

    -- generated the title and card for sudoku app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
+import { Content, Title, Card } from './components';
 import { unregister } from './core';
+import { GlobalStyles, theme } from './styles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <div>Hello World</div>
-  </React.StrictMode>,
+  <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Content data-cy="content">
+        <Title data-cy="title">Sudoku</Title>
+        <Card data-cy="card">This is WIP!!</Card>
+      </Content>
+    </ThemeProvider>
+  </>,
   document.getElementById('root')
 );
 
